b886ee640f9a15fd201d305e8d5bd8a61cab61be
AG72 <acuario7203@gmail.com>
2025-07-04 20:58:50 -0400

    FEAT[#SCRUM-32]: Consultar_Permisos_Rol

---
+++ Usuario_Application/Queries/Query_Obtener_Roles_y_Permisos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Usuario_Application.DTOs;
+using Usuario_Domain.Entities;
+
+namespace Usuario_Application.Queries;
+
+public class Query_Obtener_Roles_y_Permisos : IRequest<List<Dto_Rol_Con_Permisos>> { }
